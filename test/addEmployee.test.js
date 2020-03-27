72e2a8708193a05214a7b6852e3fbdb028ca0433
Jean Vasconcelos <jean.vasconcelos@betrybe.com>
2020-03-27 14:32:00 -0300

    fix missing trailing comma

--- test/addEmployee.test.js
+++ test/addEmployee.test.js
@@ -18,11 +18,11 @@ describe('addEmployee', () => {
     zoo.addEmployee('4141da1c-a6ed-4cf7-90c4-99c657ba4ef3', 'Jane', 'Doe',
       [
         '546fe3d4-2d81-4bb4-83a7-92d5b7048d17',
-        'a67a36ee-3765-4c74-8e0f-13f881f6588a'
+        'a67a36ee-3765-4c74-8e0f-13f881f6588a',
       ],
       [
         'ee6139bf-b526-4653-9e1e-1ca128d0ad2e',
-        '210fcd23-aa7b-4975-91b7-0230ebb27b99'
+        '210fcd23-aa7b-4975-91b7-0230ebb27b99',
       ]);
 
     assert.equal(data.employees.length, 10);
@@ -33,11 +33,11 @@ describe('addEmployee', () => {
     assert.deepEqual(lastEmployee.managers,
       [
         '546fe3d4-2d81-4bb4-83a7-92d5b7048d17',
-        'a67a36ee-3765-4c74-8e0f-13f881f6588a'
+        'a67a36ee-3765-4c74-8e0f-13f881f6588a',
       ]);
     assert.deepEqual(lastEmployee.responsibleFor, [
       'ee6139bf-b526-4653-9e1e-1ca128d0ad2e',
-      '210fcd23-aa7b-4975-91b7-0230ebb27b99'
+      '210fcd23-aa7b-4975-91b7-0230ebb27b99',
     ]);
 
     assert.equal(data.employees.length, 10);
